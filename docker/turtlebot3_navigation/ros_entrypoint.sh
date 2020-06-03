7cafa7ad1a4faf6df3541af0d94cd392917203e0
Nick Ng <ngyewch@gmail.com>
2020-06-03 17:47:28 +0800

    updated Docker build

--- docker/turtlebot3_navigation/ros_entrypoint.sh
+++ docker/turtlebot3_navigation/ros_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # setup ros environment
-source "$ROS2_WS/install/local_setup.bash"
+source "/opt/ros/dashing/setup.bash"
 source "$DEPENDENCIES_WS/install/local_setup.bash"
 source "$APP_WS/install/local_setup.bash"
 exec "$@"
